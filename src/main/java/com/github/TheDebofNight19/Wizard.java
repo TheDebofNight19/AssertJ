1d87f5521556b284d5f0f1cf20707b6bc77e2e73
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-09 12:50:33 +0300

    AssertJ Test added

--- src/main/java/com/github/TheDebofNight19/Wizard.java
+++ src/main/java/com/github/TheDebofNight19/Wizard.java
@@ -1,12 +1,24 @@
 package com.github.TheDebofNight19;
-import java.util.Set;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Wizard
 {
     private String name;
     private String house;
     private int age;
-    private Set <String> wands;
+    private BigDecimal goldInherited;
+    private HashMap<Integer,String> wands;
+
+    public Wizard(String name, String house, int age, BigDecimal goldInherited, HashMap<Integer, String> wands) {
+        this.name = name;
+        this.house = house;
+        this.age = age;
+        this.goldInherited = goldInherited;
+        this.wands = wands;
+    }
 
     public String getName() {
         return name;
@@ -20,22 +32,11 @@ public class Wizard
         return age;
     }
 
-    public Set<String> getWands() {
+    public BigDecimal getGoldInherited() {
+        return goldInherited;
+    }
+
+    public HashMap<Integer, String> getWands() {
         return wands;
-    }
-
-    public Wizard() {
-    }
-
-    public Wizard(String name, String house, int age, Set<String> wands) {
-        this.name = name;
-        this.house = house;
-        this.age = age;
-        this.wands = wands;
-    }
-
-    public static void main(String[] args )
-    {
-        System.out.println( "Hello World!" );
     }
 }
--- src/test/java/com/github/TheDebofNight19/AssertJ.java
+++ src/test/java/com/github/TheDebofNight19/AssertJ.java
@@ -3,6 +3,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import static org.assertj.core.api.Assertions.*;
+
+
 public class AssertJ {
     private static final Logger LOG = LoggerFactory.getLogger(AssertJ.class);
 
@@ -14,10 +19,18 @@ public class AssertJ {
 
     @Test
     public void test1(){
-        LOG.info("This is a testNG test!");
+
+        LOG.info("This is a Wizard assertion test!");
+        BigDecimal goldInherited = new BigDecimal(80632.2).setScale(3,BigDecimal.ROUND_DOWN);
+        HashMap<Integer, String> wands = new HashMap<>();
+        wands.put(13, "Dragon heart string");
+        Wizard wizard1 = new Wizard("Draco", "Slytherin", 11, goldInherited, wands);
+        assertThat(wizard1).hasFieldOrProperty("name")
+                            .hasFieldOrProperty("house")
+                            .isInstanceOf(Wizard.class)
+                            .hasFieldOrProperty("wands");
+
     }
-
-
 
     @AfterSuite
     public void afterSuite(){
